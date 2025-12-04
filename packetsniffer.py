ab1c233db1ef9a8698dee692b56b01a0ebe8c232
unknown <rom prager>
2025-12-04 15:10:02 +0200

    first commit

---
+++ packetsniffer.py
@@ -0,0 +1,54 @@
+import scapy.all as scapy
+import psutil
+
+def get_ethernet_interfaces():
+    stats = psutil.net_if_stats()
+    ethernet_ifaces = []
+
+    for name, st in stats.items():
+        lname = name.lower()
+
+
+        if "loopback" in lname:
+            continue
+
+        if not st.isup:
+            continue
+
+        if "ethernet" in lname:
+            ethernet_ifaces.append(name)
+
+    return ethernet_ifaces
+
+
+def choose_interface():
+    interfaces = get_ethernet_interfaces()
+    print('Avaliable Ethernet interfaces: \n')
+
+    for idx, iface in enumerate(interfaces):
+        print(f"  [{idx}] {iface}")
+
+    while True:
+        choice = input('Enter the number of the interface you want to sniff\n')
+    
+        if not choice.isdigit():
+            print('enter a valid number \n')
+            continue
+
+        eth = int(choice)
+        if eth < 0 or eth >= len(interfaces):
+            print(f'choose number between 0 and {len(interfaces)}')
+            continue
+
+        selected_iface = interfaces[eth]
+        print(f'you chose: {selected_iface}')
+        return selected_iface
+
+
+def sniffing():
+    capture = scapy.sniff(
+        
+    )
+
+
+sniffing()
